ba9d7f10088da2bb857e1454c4da36a4a7313b95
MFTECH-code <51761169+MFTECH-code@users.noreply.github.com>
2021-09-05 00:02:23 -0300

    Criação da classe OrderItem

---
+++ src/entities/OrderItem.java
@@ -0,0 +1,43 @@
+package entities;
+
+public class OrderItem {
+	private Integer quantity;
+	
+	// Composição preço do produto
+	private Product product;
+	private Double price = product.getPrice();
+	
+	public OrderItem() {
+		
+	}
+
+	public OrderItem(Integer quantity, Product product) {
+		this.quantity = quantity;
+		this.product = product;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	
+	public Product getProduct() {
+		return product;
+	}
+
+	public Double subTotal() {
+		return this.price * this.quantity;
+	}
+	
+}
